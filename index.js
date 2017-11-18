7ac476a565f78f4f99ea624e95b0577ac50cd596
Emily Reuman <reuman@gmail.com>
2017-11-18 22:25:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,4 @@ const newID = oldID + 1000000000
 
 const ageIsValid = Number.isInteger (currentAge)
 
-const randomNumber = Math.floor (Math.random()*20) +1
 
-const randomInteger = Math.floor ()
-
-const randomUserID =
